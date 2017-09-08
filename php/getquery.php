624461dca9c272a7c257f53ab13be627eff52c8c
maru-t <i12isimaru@yahoo.co.jp>
2017-09-09 08:00:53 +0900

    問い合わせ入力画面作成

---
+++ php/getquery.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+	<meta charset="utf8">
+	<title>Amazonもどき</title>
+</head>
+
+<body>
+<h2>
+Boatrace_crawrer<br/>
+</h2>
+<h4>
+問い合わせ
+</h4>
+
+<form method="post" action="webpage_sample.php">
+	query:<br />
+	<input type="text" name="query" size=100/><br/>
+	<input type="submit" value="実行"/>
+</form>
+
+</body>
+</html>
+
